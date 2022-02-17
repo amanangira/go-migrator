cede28648c7e5a6e6c3654bb161c70d4fdf51b33
Aman Angira <aman.angira@outlook.com>
2022-02-17 14:15:42 +0530

    Update constants to include filename component constants

--- constants.go
+++ constants.go
@@ -8,4 +8,20 @@ const (
 
 	MigrationDirectionUp MigrationDirection = iota
 	MigrationDirectionDown
+
+	FileNameDefaultExtensionDelimiter = "."
+	FileNameVersionPlaceholder        = "{version}"
+	FileNameDirectionPlaceholder      = "{direction}"
+	FileNameExtensionPlaceholder      = "{extension}"
+
+	CommandUsageHelpText = "Description:\n  Execute a migration to a specified version or the latest available version." +
+		"\n\nUsage:\n  ./main [options] [<version>] [<direction>] " +
+		"\n\nArguments:\n  version                     The version number (YYYYMMDDHHMMSS) to migrate to." +
+		"\n  direction                   The direction to apply a migration towards. Possible values - up, down" +
+		"\n\nOptions:\n      migrate                Applies all the unapplied migrations in the ascending order." +
+		"\n      up                     Rolls up the migration by one version." +
+		"\n      down                   Rolls down the migration by one version." +
+		"\n      execute                Applies the specified migration. No warning is given even if the migration is already applied. " +
+		"Usage - ./main execute <version> <direction>" +
+		"\n      version                Displays the current DB migration version."
 )
